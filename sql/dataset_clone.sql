34df7819aefb8d7b59861bc2c0621fee6449850d
Wojtek Hejna <whejna@google.com>
2023-03-22 17:23:32 +0100

    adding disclaimer

--- sql/dataset_clone.sql
+++ sql/dataset_clone.sql
@@ -1,3 +1,17 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 -- define the procedure
 CREATE OR REPLACE PROCEDURE <target_dataset>.create_dataset_clone(source_project_id STRING, source_dataset_id STRING, target_project_id STRING, target_dataset_id STRING)
 BEGIN
